c8be138094e8a839265d96512f9a46238b61c661
Mark Fischer, Jr <flyingfisch@toppagedesign.com>
2023-07-17 17:40:20 -0400

    removed private keys from source code

--- tina/config.ts
+++ tina/config.ts
@@ -6,8 +6,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: "7fc65719-80f5-4e2c-8667-53d5b6bc56ec", // Get this from tina.io
-  token: "d269eed187043016bf2cdb25282efab76a17a7f8", // Get this from tina.io
+  clientId: process.env.tinacms_client_id,
+  token: process.env.tinacms_readonly_token,
   client: { skip: true },
   build: {
     outputFolder: "admin",
